12f933bcd73b915b4e774edd64d365e9d1963334
Gabriel Pampolha <pampolhag@gmail.com>
2021-12-15 20:31:30 +0000

    reverted all changes on connectionErrorEmbed.

--- functions/errors/connectionErrorEmbed.js
+++ functions/errors/connectionErrorEmbed.js
@@ -1,18 +1,12 @@
 const { User, MessageEmbed } = require('discord.js');
-const { client } = require('../../index');
 
-const connectionErrorEmbed = (err, message = undefined) =>
+const connectionErrorEmbed = async (err, message = undefined) =>
 {
     console.error(err);
 
     const embed = new MessageEmbed()
     .setColor('RED')
     .setDescription('*Ops! Parece que algo deu errado ao tentar pegar as informações...*');
-
-    if (err.toString().toLowercase().contains('miniget error'))
-    {
-      new User(client, { id: process.env.meuID }).send('erro: ' + err.toString());
-    }
 
     if (message) return message.channel.send(embed);
     else return embed;
